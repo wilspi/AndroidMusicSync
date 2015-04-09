ed86c5b8fd87a989576b50a485618e4a39972055
wilspi <sourabhdeokar@gmail.com>
2015-04-10 00:59:19 +0530

    pymtp combined

--- src/androidMusicSync.py
+++ src/androidMusicSync.py
@@ -9,10 +9,15 @@
 
 # -*- coding: cp1252 -*-
 # -*- coding: utf-8 -*-
+import os
+import re
+import shutil
+import pymtp
+
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import ID3NoHeaderError
-import mutagen.flac, mutagen.mp3
-import os,re,shutil
+import mutagen.flac
+import mutagen.mp3
 
 class bcolors:
     HEADER = '\033[95m'
@@ -25,7 +30,22 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 #destAddress="/home/batman/myZone/temp"
-destAddress="/run/user/1000/gvfs/mtp:host=%5Busb%3A002%2C010%5D/Internal storage/Music/Songs/English/Artiste"
+destAddress="/run/user/1000/gvfs/mtp:host=%5Busb%3A002%2C017%5D/Internal storage/Music/Songs/English/Artiste"
+
+#load MTP Device and do required stuff
+def loadDevice(mypath):
+    mtp = pymtp.MTP()
+    try:
+        mtp.connect()
+        print (bcolors.OKBLUE + mtp.get_manufacturer() + " : " + mtp.get_modelname() + bcolors.ENDC + " DETECTED" )    
+
+        #copying music files
+        copyMusicFiles(mypath)
+
+        mtp.disconnect()
+    except pymtp.NoDeviceConnected:
+        print (bcolors.FAIL + "ERROR : NO DEVICE DETECTED" + bcolors.ENDC )    
+
 
 #copy Music ie mp3/flac files to destAddress and arrange it by Artist/SongName
 def copyMusicFiles(mypath):
@@ -54,18 +74,23 @@ def copyMusicFiles(mypath):
             try:
                 print ("COPYING " + bcolors.OKBLUE + str(audioMeta["title"][0].encode('ascii','ignore')) + bcolors.ENDC )
                 tempDir=os.path.join(destAddress,(str(audioMeta["Artist"][0].encode('ascii','ignore'))))              
+                
                 if not os.path.exists(tempDir):
-                    os.makedirs(tempDir)
+                    print(tempDir)
+                    #os.makedirs(tempDir,0777)
 
                 if(file.lower().endswith('.flac')):
                     destFile=os.path.join(tempDir,(str(audioMeta["Title"][0].encode('ascii','ignore'))))+".flac"
                 elif(file.lower().endswith('.mp3')):
                     destFile=os.path.join(tempDir,(str(audioMeta["Title"][0].encode('ascii','ignore'))))+".mp3"
                 if not os.path.exists(destFile):
-                    shutil.copyfile(file, destFile)
+                    pass
+                    #shutil.copyfile(file, destFile)
+                    #mtp.send_file_from_file(file, destFile, parent=0)
 
             except KeyError:
                 print (bcolors.FAIL + "ERROR : FILE DIDNT COPY" + bcolors.ENDC )    
+
 
 #driver function
 def main():
@@ -76,7 +101,8 @@ def main():
     if(len(mypath)==0):
 		mypath = "."
     if(os.path.exists(mypath)):
-        copyMusicFiles(mypath)
+        loadDevice(mypath)
+        #copyMusicFiles(mypath)
     else:
         print (bcolors.FAIL + "ERROR : NO SUCH PATH" + bcolors.ENDC )    
     
